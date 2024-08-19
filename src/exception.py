8952274a347de7ce8dabdd5ce9a41666155abfb6
himavarthanML <himamachinelearning@gmail.com>
2024-08-18 22:09:20 -0500

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -11,12 +11,12 @@ def error_message_details(error,error_detail:sys):
      )
     return error_message
 
-    class CustomException(Exception):
-        def __init__(self,error_message,error_detail:sys):
-            super().__init__(error_message)
-            self.error_message=error_message_details(error_message,error_detail=error_detail)
+class CustomException(Exception):
+       def __init__(self,error_message,error_detail:sys):
+              super().__init__(error_message)
+              self.error_message=error_message_details(error_message,error_detail=error_detail)
 
-        def __str__(self):
+       def __str__(self):
             return self.error_message
         
 #if __name__=="__main__":
@@ -25,6 +25,6 @@ def error_message_details(error,error_detail:sys):
 #                a=1/0
 #     except Exception as e:
 #                logging.info("Divide by Zero")
-#                raise CustomException(e,sys) # type: ignore
+#                raise CustomException(e,sys) 
                 
     
