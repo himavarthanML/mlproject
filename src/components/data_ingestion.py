e80bc956c9d1acc4a89ac4888b129a15f3184de2
himavarthanML <himamachinelearning@gmail.com>
2024-08-19 21:44:23 -0500

    Data Transformation Completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from data_transformation import DataTransformation
+from data_transformation import DataTransformationConfig
 
 
 @dataclass
@@ -46,7 +48,10 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
 
 
 
